09a598f8c37c20136a9ee5576ead19d9cb86bb4b
Jonathan Berthias <jvberthias@gmail.com>
2025-07-14 21:19:56 +0200

    Add global interface that dispatches to the solvers (#163)
    The idea is to be able to use the high-level interface without importing
    from a solver subpackage. The high-level interface then simply redirects
    to the appropriate function in the solver packages.
    
    Care was taken to make sure all files in the global package can be
    imported without having `gurobipy` or `pyscipopt` installed. Then we
    will try to import the solvers only once one is requested explicitly.

--- src/cvxpy_translation/__init__.py
+++ src/cvxpy_translation/__init__.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 from cvxpy_translation._version import __version__ as __version__
 from cvxpy_translation._version import __version_tuple__ as __version_tuple__
 from cvxpy_translation.exceptions import InvalidParameterError as InvalidParameterError
@@ -8,3 +10,15 @@ from cvxpy_translation.exceptions import UnsupportedError as UnsupportedError
 from cvxpy_translation.exceptions import (
     UnsupportedExpressionError as UnsupportedExpressionError,
 )
+from cvxpy_translation.interface import backfill_problem as backfill_problem
+from cvxpy_translation.interface import build_model as build_model
+from cvxpy_translation.interface import fill_model as fill_model
+from cvxpy_translation.interface import (
+    register_translation_solver as register_translation_solver,
+)
+from cvxpy_translation.interface import solve as solve
+
+try:
+    CVXPY_VERSION = tuple(map(int, importlib.metadata.version("cvxpy").split(".")))
+except importlib.metadata.PackageNotFoundError:
+    CVXPY_VERSION = tuple(map(int, importlib.metadata.version("cvxpy-base").split(".")))
